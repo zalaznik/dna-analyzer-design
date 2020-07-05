b79ec3ddec87f597d0ab1b70fe49f642c75a1c0e
Tami Brecher <z618149947@gmail.com>
2020-07-05 16:40:46 +0300

    add class dnaSequence

---
+++ src/meta_data/dna_seq.cpp
@@ -0,0 +1,152 @@
+
+#include "dna_seq.h"
+
+static bool IsValidDna(std::string dna)
+{
+    size_t i = 0;
+
+    if(dna[i] == '\0')
+        return false;
+
+    while (dna[i] != '\0')
+    {
+        if(dna[i]!='A' and dna[i]!='C' and dna[i]!='T' and dna[i]!='G')
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+
+DnaSequence::DnaSequence(const char *dna_Sequence):m_dna_sequence(NULL)
+{
+
+    if (IsValidDna(dna_Sequence))
+    {
+
+        m_length = strlen(dna_Sequence);
+
+        m_dna_sequence = new Nucleotide[m_length];
+
+        for( size_t i = 0 ; i<m_length ; i++)
+        {
+            m_dna_sequence[i] = dna_Sequence[i];
+        }
+    }
+
+    else
+    {
+        std::cout<<"invalid DNA, an empty object will create!"<<std::endl;
+    }
+}
+
+
+DnaSequence::DnaSequence(const std::string& dna_Sequence):m_dna_sequence(NULL)
+{
+    if (IsValidDna(dna_Sequence))
+    {
+        m_dna_sequence = new Nucleotide[dna_Sequence.length()];
+        size_t  i = 0;
+
+        while (dna_Sequence[i]!='\0')
+        {
+            m_dna_sequence[i] = dna_Sequence[i];
+            i++;
+        }
+        m_length = i;
+    }
+
+    else
+    {
+        std::cout<<"invalid DNA, an empty object will create!"<<std::endl;
+    }
+}
+
+
+DnaSequence::DnaSequence(const DnaSequence& dna_sequence):m_dna_sequence(NULL)
+{
+
+    m_dna_sequence = new Nucleotide[dna_sequence.length()];
+    size_t i;
+
+    for (i = 0 ; i<dna_sequence.length(); i++)
+    {
+        m_dna_sequence[i] = dna_sequence.m_dna_sequence[i].m_nucleotide;
+        i++;
+    }
+    m_length = i;
+}
+
+
+DnaSequence::~DnaSequence()
+{
+    delete[] m_dna_sequence;
+}
+
+
+DnaSequence& DnaSequence::operator=(const DnaSequence& dna_sequence)
+{
+    if(strcmp(getMDnaSequence(), dna_sequence.getMDnaSequence()) == 0)
+    {
+        return *this;
+    }
+
+    if(length()!=dna_sequence.length())
+    {
+        Nucleotide* temp = new Nucleotide[dna_sequence.length()];
+        delete[] m_dna_sequence;
+        m_dna_sequence = temp;
+    }
+
+    else
+    {
+        delete[] m_dna_sequence;
+    }
+
+    size_t  i = 0;
+
+    while ( (dna_sequence.getMDnaSequence())[i]!='\0')
+    {
+        m_dna_sequence[i].m_nucleotide = dna_sequence.getMDnaSequence()[i];
+        i++;
+    }
+    m_length = dna_sequence.length();
+    return *this;
+
+}
+
+
+DnaSequence::Nucleotide& DnaSequence::operator[](size_t index) const
+{
+    if (index >= length())
+    {
+        std::cout << "index out of the range" << std::endl;
+        //throw;
+    }
+
+    return m_dna_sequence[index];
+}
+
+
+size_t  DnaSequence::length() const
+{
+    return m_length;
+}
+
+
+const char* DnaSequence::getMDnaSequence() const
+{
+    char* char_dna_sequence = new char[length()+1];
+    size_t i;
+
+    for (i = 0 ; i<length(); i++)
+    {
+        char_dna_sequence[i] = m_dna_sequence[i].m_nucleotide;
+    }
+    char_dna_sequence[i] = '\0';
+
+    return char_dna_sequence;
+}
+
---
+++ src/meta_data/dna_seq.h
@@ -0,0 +1,53 @@
+
+
+#ifndef __DNA_SEQ_H__
+#define __DNA_SEQ_H__
+
+
+class DnaSequence{
+
+private:
+    class  Nucleotide
+    {
+    public:
+        Nucleotide(){};
+        //Nucleotide(char nucleotide):m_nucleotide(nucleotide){};
+
+        Nucleotide& operator= (char nucleotide)
+        {
+            if(nucleotide!='A' and nucleotide!='C' and nucleotide!='T' and nucleotide!='G')
+            {
+                std::cout<<"invalid nocleotide"<<std::endl;
+                //throw
+            }
+
+            m_nucleotide = nucleotide;
+            return *this;
+        };
+
+        char getMatchNucleotide() const;
+
+        char m_nucleotide;
+
+    };
+
+    Nucleotide* m_dna_sequence;
+    size_t  m_length;
+
+
+public:
+    DnaSequence(){}
+    DnaSequence(const char* dna_sequence);
+    DnaSequence(const std::string& dna_sequence);
+    DnaSequence(const DnaSequence& dna_sequence);
+    ~DnaSequence();
+    DnaSequence& operator=(const DnaSequence& dna_sequence);
+    DnaSequence::Nucleotide& operator[](size_t index) const;
+    size_t length() const;
+    const char* getMDnaSequence() const;
+
+};
+
+
+
+#endif
