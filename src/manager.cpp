87163bf5f070c4849ea1caed00d3a31837dfdece
Zalaznik <zc@sem-wolf.org>
2020-07-09 01:05:22 +0300

    create Manager class with runManager class

---
+++ src/manager.cpp
@@ -0,0 +1,15 @@
+//
+// Created by a on 7/8/20.
+//
+
+#include "manager.h"
+
+void Manager::runManager(IReader* reader, IWriter* writer)
+{
+    Terminal terminal(reader, writer);
+
+    while (1)//to change to quit command
+    {
+        terminal.runTerminal();
+    }
+}
---
+++ src/manager.h
@@ -0,0 +1,18 @@
+//
+// Created by a on 7/8/20.
+//
+
+#ifndef SRC_MANAGER_H
+#define SRC_MANAGER_H
+
+#include "terminal.h"
+
+
+class Manager {
+public:
+    void runManager(IReader* reader, IWriter* writer);
+
+};
+
+
+#endif //SRC_MANAGER_H
