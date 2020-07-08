02d639b6c09427512ef49085212512fe58f6fe56
Zalaznik <zc@sem-wolf.org>
2020-07-09 02:38:35 +0300

    add a call to Parser::parse method

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -3,12 +3,13 @@
 //
 
 #include "terminal.h"
+#include "utils/parser.h"
 
 void Terminal::runTerminal()
 {
     m_writer->write(" > cmd >>> ");
-    m_reader->read();
-    //to parse what return value from read
+    Parser parser;
+    parser.parse(m_reader->read(), " ");
     //to send to Factory which decide what command to call to
     //to call to the command which Factory return
 
