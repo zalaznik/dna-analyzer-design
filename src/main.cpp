1e930e4fe29a5078462ea54203422a1b33303e68
Zalaznik <zc@sem-wolf.org>
2020-07-09 00:25:29 +0300

    create a call to startApp.start

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+#include "start_app.h"
+#include "reader/keyboard_reader.h"
+#include "writer/screen_writer.h"
+
+int main()
+{
+    KeyboardReader key_board_reader;
+    ScreenWriter screen_writer;
+
+    StartApp start_app;
+    start_app.start(&key_board_reader, &screen_writer);
+
     return 0;
 }
