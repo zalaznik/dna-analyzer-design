8680a71ed71caafd1558c2a2181745be7138c272
Zalaznik <zc@sem-wolf.org>
2020-09-17 18:29:30 +0300

    add try & catch in the func runTerminal

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -5,11 +5,25 @@
 #include "terminal.h"
 #include "utils/parser.h"
 #include "factory.h"
+#include <exception> //for std::exception
+#include <iostream> // for std::cout
+
+
 
 void Terminal::runTerminal()
 {
     m_writer->write(" > cmd >>> ");
     Parser parser;
     Factory factory;
-    (factory.makeFactory(parser.parse(m_reader->read(), " ")))->doAction();
+
+    try
+    {
+        (factory.makeFactory(parser.parse(m_reader->read(), " "))) -> doAction();
+    }
+
+    catch(const std::exception &e)
+    {
+        std::cout << e.what() << "please try again " << std::endl;
+    }
 }
+
