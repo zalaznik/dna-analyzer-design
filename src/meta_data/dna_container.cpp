4dc5a84155448461fcb7efd66731c656f4a81ae1
Zalaznik <zc@sem-wolf.org>
2020-09-17 18:17:09 +0300

    add the method searchDnaById

--- src/meta_data/dna_container.cpp
+++ src/meta_data/dna_container.cpp
@@ -1,6 +1,10 @@
 
 #include "dna_container.h"
 
+#include <exception> //for std::exception
+#include <iostream> //for std
+
+
 std::map<size_t, DnaData*> DnaContainer::idMap;
 std::map<const char* , size_t> DnaContainer::nameIdMap;
 
@@ -8,4 +12,17 @@ void DnaContainer::addToMap(DnaData* dna_data)
 {
     idMap.insert(std::pair<size_t, DnaData*>(dna_data->getId(), dna_data));
     nameIdMap.insert(std::pair<const char* , size_t>(dna_data->getName(), dna_data->getId()));
+}
+
+
+DnaData* DnaContainer::searchDnaById(size_t id_for_search)
+{
+    std::map<size_t, DnaData*>::iterator itr = idMap.find(id_for_search);
+
+    if(itr == idMap.end())
+    {
+        throw std::invalid_argument("this ID isn't exist\n");
+    }
+
+    return itr->second;
 }
--- src/meta_data/dna_container.h
+++ src/meta_data/dna_container.h
@@ -10,6 +10,7 @@ class DnaContainer
 {
 public:
     static void addToMap(DnaData* dna_data);
+    static DnaData* searchDnaById (size_t id_for_search);
 
 private:
     static std::map<size_t, DnaData*> idMap;
